ffb37d74eec6a645174067583701d6d41caaa586
Geerozero <52261196+Geerozero@users.noreply.github.com>
2020-03-22 21:37:23 -0400

    Basic shop UI together

---
+++ Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using TMPro;
+
+public class InventoryManager : MonoBehaviour
+{
+    static public int money = 0;
+    public TextMeshProUGUI moneyUIText;
+
+    private void Start()
+    {
+        UpdateText();
+    }
+    public void AddMoney(int increaseAmount)
+    {
+        money += increaseAmount;
+        UpdateText();
+    }
+
+    public void SubtractMoney(int decreaseAmount)
+    {
+        money -= decreaseAmount;
+        UpdateText();
+    }
+
+    private void UpdateText()
+    {
+        moneyUIText.SetText("Money: " + money);
+    }
+}
---
+++ Assets/Scripts/MarketManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarketManager : MonoBehaviour
+{
+    public GameObject BuyContainer;
+    public GameObject SellContainer;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void DisplayBuy()
+    {
+        BuyContainer.SetActive(true);
+        SellContainer.SetActive(false);
+    }
+
+    public void DisplaySell()
+    {
+        BuyContainer.SetActive(false);
+        SellContainer.SetActive(true);
+    }
+
+
+}
