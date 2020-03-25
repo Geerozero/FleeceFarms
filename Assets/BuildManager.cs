d2abd95650131a7f7284f69178f4bcf07b96866c
Josh Rain <joshrain@knights.ucf.edu>
2020-03-24 21:42:27 -0400

    lots to do with upgrades

--- Assets/BuildManager.cs
+++ Assets/BuildManager.cs
@@ -34,11 +34,28 @@ public class BuildManager : MonoBehaviour
 
     public void selectPen(node node)
     {
+        if (selectedPen == node)
+        {
+            DeselectNode();
+            return;
+
+        }
         selectedPen = node;
         //penToBuild = null;
         nodeUI.SetTarget(node);
     }
+    public void DeselectNode()
+    {
+        selectedPen = null;
+        nodeUI.Hide();
+    }
+    public void SelectAnimalPenToBuild (AnimalPenBluePrint pen)
+    {
+        penToBuild = pen;
+        selectedPen = null;
 
+        nodeUI.Hide();
+    }
     //add type of pen to select "level of pen"
     //selectedpen = null;
 }
--- Assets/node.cs
+++ Assets/node.cs
@@ -8,7 +8,9 @@ public class node : MonoBehaviour {
     public Vector3 positionOffset;
 
     private GameObject animalPen;
-    
+    public bool isUpgraded = false;
+
+    public AnimalPenBlueprint animalPenBlueprint;
     private Color startColor;
     
     private Renderer rend;
@@ -16,6 +18,9 @@ public class node : MonoBehaviour {
 void Start ()
 {
     rend = GetComponent<Renderer>();
+    startColor = rend.material.color;
+
+    buildManager = BuildManager.instance;
 }
 
     void OnMouseDown()
@@ -26,21 +31,49 @@ void Start ()
 
         if (animalPen != null)
         {
+            
             buildManager.selectPen(this);
             Debug.Log("cant build here - to do display on screen");
+            
             return;
 
+
         }
+        if (!buildManager.CanBuild)
+            return;
+
+        BuildAnimalPen(buildManager.GetPenToBuild());
 
 
         //build animalpen
-        GameObject penToBuild = BuildManager.instance.GetPenToBuild();
-        animalPen = (GameObject)Instantiate(penToBuild, transform.position + positionOffset, transform.rotation);
+        //GameObject penToBuild = BuildManager.instance.GetPenToBuild();
+        //animalPen = (GameObject)Instantiate(penToBuild, transform.position + positionOffset, transform.rotation);
 
     }
+    void BuildAnimalPen (AnimalPenBlueprint blueprint)
+    {
+        if (InventoryManager.money < penToBuild.cost)
+        {
+            Debug.Log("you poor");
+            return;
+        }
+
+        InventoryManager.money -= blueprint.cost;
+        GameObject _pen = (GameObject)Instantiate(blueprint.prefab, GetBuildPosition(), Quaterion.identity);
+        animalPen = _pen;
+
+        AnimalPenBlueprint = blueprint;
+
+        //add effect
+
+        Debug.
+    }
+
+    
     void OnMouseEnter()
     {
-    rend.material.color = hoverColor;
+        
+        rend.material.color = hoverColor;
     }
 
     void OnMouseExit()
