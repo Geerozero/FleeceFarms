c9cea5cdccfd0412f5ab3e0e3ddd18fb45fef0b6
Geerozero <52261196+Geerozero@users.noreply.github.com>
2020-04-09 22:15:44 -0400

    edited title aspects, it loads farm

---
+++ Assets/Scripts/TitleManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleManager : MonoBehaviour
+{
+    [Header("Current name of Farm Scene")]
+    public string FarmSceneName;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(FarmSceneName);
+    }
+
+
+    public void Settings()
+    {
+        Debug.Log("Sound settings/Window scale here");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
