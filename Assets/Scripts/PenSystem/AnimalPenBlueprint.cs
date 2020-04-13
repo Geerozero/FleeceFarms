c8d66c08b947cce8550af70079cd81a737db0080
Josh-Rain <59661114+Josh-Rain@users.noreply.github.com>
2020-04-12 20:06:24 -0400

    f

--- Assets/Scripts/PenSystem/AnimalPenBlueprint.cs
+++ Assets/Scripts/PenSystem/AnimalPenBlueprint.cs
@@ -3,10 +3,11 @@ using System.Collections;
 
 
 [System.Serializable]
-public class AnimalPenBlueprint 
+public class AnimalPenBlueprint : MonoBehaviour
 {
     public GameObject prefab;
     public int cost;
+    public int outPut;
 
     public GameObject upgradedPrefab;
     public int upgradeCost;
